a7ab316a6a135b0b2e74bbf1aa59d657992d4c58
sumeetmohite <sumeetmohite9@gmail.com>
2021-02-04 22:50:32 +0530

    first commit

---
+++ db/schema.rb
@@ -0,0 +1,14 @@
+
+ActiveRecord::Schema.define(version: 2021_02_03_125655) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
